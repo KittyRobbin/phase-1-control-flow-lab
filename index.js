4e6d0a61e3c6f76328c4f9bd7d282ce3caf55d07
Elizabeth <elizabeth.muthoni@student.moringaschool.com>
2023-09-24 02:52:07 +0300

    done

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if (ride <=400){
+return 'This one is on me!'
+
+} else if (ride>2000 & ride<=2500){
+return 'I will gladly take your thirty bucks.' 
+
+} else{
+  return 'No can do.'
+
+}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if (city==="NYC"){
+    return 'Ok, sounds good.'
+
+  } else{city==="Pittsburgh"}
+  return 'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(b) {
+  switch (b) {
+    case 'generous':
+    return 'Thank you so much.';
+   case 'not so generous':
+   return 'Thank you.';
+   default :
+    return 'Bye.';
+  } 
 }
